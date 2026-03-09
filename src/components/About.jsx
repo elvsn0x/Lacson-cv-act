318b172813403d19870b1723907e4c68dd51ba9d
Erin Lacson <lacson.erin18@gmail.com>
2026-03-09 21:12:20 +0800

    Fix: Profile photo not showing

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,10 @@
 import Card from "./Card";
+import profileImage from "../assets/profile.jpeg";  // ADD THIS
 
 function About() {
   return (
     <Card title="About Me">
-      <img src="public/images/profile.jpeg" alt="Profile photo of Erin B. Lacson" width="250" />
+      <img src={profileImage} alt="Profile photo of Erin B. Lacson" width="250" />
       <p>Creative and detail-oriented designer with a passion for crafting intuitive user experiences and visually engaging interfaces. Skilled in UI/UX design, visual storytelling, and modern web development practices.</p>
       <p>My career goal is to design products that not only look beautiful but also function seamlessly, allowing me to collaborate effectively with developers, product managers, and clients.</p>
     </Card>
